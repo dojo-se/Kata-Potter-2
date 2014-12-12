bc7a757fd080bb6612f5993b0959396fe6347fb6
Roberto Brandini <dp.sistemas@gmail.com>
2014-12-11 21:56:40 -0300

    aqui ainda nao tem desconto!

--- Livraria.py
+++ Livraria.py
@@ -3,10 +3,19 @@ import unittest
 def livraria(livros):
     preco = 42
     valor = 0
+    total_livros=0
+    desconto = [1,0.95,0.90,0.85,0.80]
     
     for exemplares in livros[0:7]:
         valor += exemplares * preco
+        if exemplares != 0: 
+            total_livros += 1
     
+    #print(total_livros)
+    if total_livros > 0:
+        valor_descontado = desconto[total_livros-1] * valor 
+        valor = valor_descontado
+
     return valor
 
 class LivrariaTest(unittest.TestCase):
@@ -27,6 +36,15 @@ class LivrariaTest(unittest.TestCase):
         
     def testar_oitavo_livro(self):
         self.assertEqual(0, livraria([0,0,0,0,0,0,0,1]))
+    
+    def test_dois(self):
+        self.assertEqual(84, livraria([2,0,0,0,0,0,0]))
+        self.assertEqual(84, livraria([0,2,0,0,0,0,0]))
+        self.assertEqual(84, livraria([0,0,2,0,0,0,0]))
+        self.assertEqual(84, livraria([0,0,0,2,0,0,0]))
+        self.assertEqual(84, livraria([0,0,0,0,2,0,0]))
+        self.assertEqual(84, livraria([0,0,0,0,0,2,0]))
+        self.assertEqual(84, livraria([0,0,0,0,0,0,2]))
 
 
 
